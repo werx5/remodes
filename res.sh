1f2670c53ca9fcfa7073647f7a3c3aa545e13287
werx5 <95148930+werx5@users.noreply.github.com>
2022-02-22 17:54:57 +0300

    Update res.sh

--- res.sh
+++ res.sh
@@ -1,8 +1,29 @@
+#!/bin/bash
+    slm=$(echo "\033[31;3m")
+    normal=$(echo "\033[m")
+    menu=$(echo "\033[36m") #Blue
+    number=$(echo "\033[33m") #yellow
+    bgred=$(echo "\033[39;3m")
+    fgred=$(echo "\033[32;3m")
+    kbred=$(echo "\033[33;3m")
+    printf "\n${kbred}-----> Kurulum ilerliyor${normal}\n"
+#------------------------------------------------
+# Setup Ubuntu 16.04 with xRDP via MATE-Desktop
+#------------------------------------------------
+#https://gist.github.com/rvido/36c6af244e3e0989df8bd30189090635 ---> is a quote
+
 apt update
-apt install xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils -y
 apt install xrdp -y
-systemctl status xrdp
-adduser xrdp ssl-cert  
-systemctl restart xrdp
-ufw allow from 192.168.1.0/24 to any port 3389
-ufw allow 3389
+apt install mate-core mate-desktop-environment mate-notification-daemon -y
+sed -i.bak '/fi/a #xrdp multiple users configuration \n mate-session \n' /etc/xrdp/startwm.sh -y
+systemctl enable xrdp
+systemctl start xrdp
+
+    printf "\n${fgred}-----> Kurulum Tamamlandı!${normal}\n"
+
+    printf "\n${slm}-----> Firefox Tarayıcı Yükleniyor...${normal}\n"
+    sleep 2
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A6DCF7707EBC211F
+apt-add-repository "deb http://ppa.launchpad.net/ubuntu-mozilla-security/ppa/ubuntu bionic main"
+apt update
+apt install firefox
