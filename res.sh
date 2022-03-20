3d78c0b38eb169f13261f9cdacb59397012d09f2
werx5 <95148930+werx5@users.noreply.github.com>
2022-03-20 11:12:29 +0300

    Update res.sh

--- res.sh
+++ res.sh
@@ -8,22 +8,19 @@
     kbred=$(echo "\033[33;3m")
     printf "\n${kbred}-----> Kurulum ilerliyor${normal}\n"
 #------------------------------------------------
-# Setup Ubuntu 16.04 with xRDP via MATE-Desktop
+# Debian Uzak Masaüstü Kurulumu
 #------------------------------------------------
-#https://gist.github.com/rvido/36c6af244e3e0989df8bd30189090635 ---> is a quote
 
-apt update
-apt install xrdp -y
-apt install mate-core mate-desktop-environment mate-notification-daemon -y
-sed -i.bak '/fi/a #xrdp multiple users configuration \n mate-session \n' /etc/xrdp/startwm.sh -y
-systemctl enable xrdp
-systemctl start xrdp
+sudo apt update
 
-    printf "\n${fgred}-----> Kurulum Tamamlandı!${normal}\n"
+sudo apt -y install xrdp
 
-    printf "\n${slm}-----> Firefox Tarayıcı Yükleniyor...${normal}\n"
-    sleep 2
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A6DCF7707EBC211F
-apt-add-repository "deb http://ppa.launchpad.net/ubuntu-mozilla-security/ppa/ubuntu bionic main"
-apt update
-apt install firefox
+sudo adduser xrdp ssl-cert
+
+sudo ufw allow 3389
+
+sudo systemctl restart xrdp
+
+sudo apt install task-xfce-desktop
+
+echo "bitti"
