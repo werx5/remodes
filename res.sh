8255c21e9957fb48bca7b2b40596c20d97e45d7b
werx5 <95148930+werx5@users.noreply.github.com>
2022-03-20 12:13:32 +0300

    Update res.sh

--- res.sh
+++ res.sh
@@ -21,6 +21,6 @@ sudo ufw allow 3389
 
 sudo systemctl restart xrdp
 
-sudo apt -y install task-lxqt-desktop
+sudo apt -y install task-lxde-desktop
 
     printf "\n${slm}-----> Kurulum Bitti....${normal}\n"
